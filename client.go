be71f736c3d2e63edb8e29b15bb6f435047d6330
radu-matei <matei.radu94@gmail.com>
2017-07-25 06:25:09 +0300

    Add initial go OMS client (from https://github.com/Azure/oms-log-analytics-firehose-nozzle/blob/master/client/oms_client.go)
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ client.go
@@ -0,0 +1,97 @@
+package omsclient
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	method      = "POST"
+	contentType = "application/json"
+	resource    = "/api/logs"
+)
+
+// client posts messages to OMS
+type client struct {
+	customerID      string
+	sharedKey       string
+	url             string
+	httpPostTimeout time.Duration
+}
+
+// Client interface
+type Client interface {
+	PostData(*[]byte, string) error
+}
+
+// NewOmsClient object
+func NewOmsClient(customerID string, sharedKey string, postTimeout time.Duration) Client {
+	return &client{
+		customerID:      customerID,
+		sharedKey:       sharedKey,
+		url:             "https://" + customerID + ".ods.opinsights.azure.com" + resource + "?api-version=2016-04-01",
+		httpPostTimeout: postTimeout,
+	}
+}
+
+func (c *client) PostData(msg *[]byte, logType string) error {
+	// Headers
+	contentLength := len(*msg)
+	rfc1123date := time.Now().UTC().Format(time.RFC1123)
+	//TODO: rfc1123 date should have UTC offset
+	rfc1123date = strings.Replace(rfc1123date, "UTC", "GMT", 1)
+	//Signature
+	signature, err := c.buildSignature(rfc1123date, contentLength, method, contentType, resource)
+	if err != nil {
+		return err
+	}
+	// Create request
+	req, err := http.NewRequest("POST", c.url, bytes.NewBuffer(*msg))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", signature)
+	req.Header.Set("Log-Type", logType)
+	//TODO: headers should be case insentitive
+	//req.Header.Set("x-ms-date", rfc1123date)
+	req.Header["x-ms-date"] = []string{rfc1123date}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: c.httpPostTimeout,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode >= 300 || resp.StatusCode < 200 {
+		return fmt.Errorf("Post Error. HTTP response code:%d message:%s", resp.StatusCode, resp.Status)
+	}
+	return nil
+}
+
+// initial signature builder
+// to update later
+func (c *client) buildSignature(date string, contentLength int, method string, contentType string, resource string) (string, error) {
+	xHeaders := "x-ms-date:" + date
+	stringToHash := method + "\n" + strconv.Itoa(contentLength) + "\n" + contentType + "\n" + xHeaders + "\n" + resource
+	bytesToHash := []byte(stringToHash)
+	keyBytes, err := base64.StdEncoding.DecodeString(c.sharedKey)
+	if err != nil {
+		return "", err
+	}
+	hasher := hmac.New(sha256.New, keyBytes)
+	hasher.Write(bytesToHash)
+	encodedHash := base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+	authorization := fmt.Sprintf("SharedKey %s:%s", c.customerID, encodedHash)
+	return authorization, err
+}
